32df9e0373a7279a3fb1bfcd23c867b881be76fc
Ben Purinton <ben.purinton@gmail.com>
2023-04-17 09:04:36 -0800

    Bp tabbed interface (#1)
    * small devise additions
    
    * generated users
    
    generated photos
    
    generated comments
    
    added comment model logic
    
    * generated follow_requests and added some association accessors
    
    generated likes and added some association accessors
    
    * added some counter caches
    
    * merged in photo, user, and comment validations
    
    photo validations
    
    user validations
    
    * annotated models
    
    * add annotate pre-work
    
    * add photos count to user
    
    * added some more accessors adn scope
    
    * added user leader follower feed discover
    
    * typo
    
    * added default to users private
    
    * scopes in photos
    
    * added enum to follow requests
    
    * added accepted scope to users
    
    * started sample_data
    
    * finally got sample data working
    
    * sample data works with create method
    
    * remove scaffold.scss
    
    * add cdn
    
    * fix navbar
    
    * links in navbar added alice to users
    
    * alice in edit link
    
    * added flash messages
    
    * add devis sanitizer
    
    * set up users#show action
    
    * added photo cards
    
    * user show page looking good
    
    * update controller to username
    
    * liked photos page working, going to refactor
    
    * photo card partials
    
    * create profile navigation partial
    
    * just testing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '6bcbebdb8111bcba88b8342afae0c47506a6d5146052a921b66e2414c87ede44ea9e806c16ffde4ca9c86aeaa04ab27eaaea22efc2245ea416985ee2782370e3'
+  # config.secret_key = 'd36f0a75f5a1463ca32f46708f16dd43aa079dc9d7d8880bf914eacdbfdb48ed0dd93bf3be0143047fec952b70dc30aaea0eaa3dd9c6d0aa8a2b53b236bc291a'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '8db525c51180cfc2e6be2c863b8297cd6648ffffaf8b7f8229f69e724eff7040443da2c3212e5284e643dc673a4946ad49b824bb113fd225a6c4805333df9085'
+  # config.pepper = '22a77d0ce04997114328bf5f6d6ffa4af6ab45306ae8deff733f91a7037812fe26c2ce47bdce0a753a4acf93c9df522c2537b907f2dd4601f66f2e075aeb9557'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
